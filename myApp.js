662221fa8098eab38d58659799da3250232557cf
Saili <natu2@wisc.edu>
2024-08-08 18:16:45 -0700

    Challenges: Start a working express server, serve an HTML file, serve static assets, serve JSON on a specific route, use the .env file, implement a root-level request logget middleware

--- myApp.js
+++ myApp.js
@@ -1,39 +1,30 @@
-let express = require('express');
+//load environment variables from .env to process.env
+require("dotenv").config();
+
+let express = require("express");
 let app = express();
+console.log("Hello World");
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  //next() allows the server to continue to the next function in the call stack
+  next();
+});
 
+//Handles all get requests
+app.get("/", (req, res) => {
+  let absolutePath = __dirname + "/views/index.html";
+  res.sendFile(absolutePath);
+});
 
+// Middleware = functions that intercept route handlers adding some kind of information
+// middleware func = express.static(path)
+app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/json", (req, res) => {
+  process.env.MESSAGE_STYLE == "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "Hello json" });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
